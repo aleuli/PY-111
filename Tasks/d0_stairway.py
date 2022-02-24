5b3fa19c6c32c75d8122090eff1c0684b9c45f57
aleksandr <alek-ulitin@yandex.ru>
2022-02-24 19:58:38 +0300

    init

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,33 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    return reverse_stairway_path(stairway)
+
+def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    count_stars = len(stairway)
+    total_coast = [0] * count_stars
+
+    total_coast[0] = stairway[0]
+    total_coast[1] = stairway[1]
+
+    for i in range(2, count_stars):
+        total_coast[i] = stairway[i] + min(total_coast[i - 1], total_coast[i - 2])
+
+    return total_coast[-1]
+
+
+def reverse_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    count_stars = len(stairway)
+    total_coast: list[float] = [float("inf")] * count_stars
+
+    total_coast[0] = stairway[0]
+    total_coast[1] = stairway[1]
+
+    for i in range(0, count_stars):
+        if i + 1 < count_stars:
+            total_coast[i + 1] = min(total_coast[i] + stairway[i + 1], total_coast[i + 1])
+
+        if i + 2 < count_stars:
+            total_coast[i + 2] = min(total_coast[i] + stairway[i + 2], total_coast[i + 2])
+
+    return total_coast[-1]
