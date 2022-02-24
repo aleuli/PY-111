28b8da48ac854b4620fc2bc955b10d1b94233bae
aleksandr <alek-ulitin@yandex.ru>
2022-02-24 21:26:29 +0300

    init

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,26 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+    rows = shape[0]  # кол-во строк отсчет с 1
+    cols = shape[1]  # кол-во столбцов отсчет с 1
+
+    def get_steps(i, j):
+        if i == 0 and j == 0:  # левый верхний угол
+            return 1
+
+        if not 0 <= i < rows:  # выпадаю за границы поля  для строк
+            return 0
+
+        if not 0 <= j < cols:  # выпадаю за границы поля для столбцов
+            return 0
+
+        return sum([get_steps(i - 2, j + 1), get_steps(i - 2, j - 1), get_steps(i - 1, j - 2), get_steps(i + 1, j - 2)])
+
+    return get_steps(i=point[0], j=point[1])
+
+
+# для оптимизации применить lru_cache
+
+if __name__ == '__main__':
+    assert 13309 == calculate_paths((7, 15), (6, 14))
+    assert 2 == calculate_paths((4, 4), (3, 3))
