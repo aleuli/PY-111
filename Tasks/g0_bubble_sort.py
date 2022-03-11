33ca207abedeb4da31bd6aa19325401bd7308702
AleUli <alek-ulitin@yandex.ru>
2022-03-11 19:11:19 +0300

    init

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -1,17 +1,34 @@
 from typing import List
+from operator import lt, gt
 
 
-def sort(container: List[int]) -> List[int]:
+def sort(container: List[int], asc: bool = True, inplace=True) -> List[int]:
     """
     Sort input container with bubble sort
-
+    :param asc если тру по возрастанию если фолс по убыванию
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    n = len(container)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if container[j] > container[j + 1]:
+    if not inplace:
+        container = container.copy()
+    offset = 1  # смещение относительно конца списка
+    compare_operator = gt if asc else lt
+    for i in range(len(container)):
+        is_exchange = False  # не было замены элементов
+        for j in range(len(container) - offset):
+            if compare_operator(container[j], container[j + 1]):
                 container[j], container[j + 1] = container[j + 1], container[j]
-
+                is_exchange = True
+        if not is_exchange:
+            break
+        offset += 1
     return container
+
+
+if __name__ == '__main__':
+    print(sort([1, 2, 3], asc=False))
+
+    list_ = [3, 2, 1]
+    sorted_copy = sort(list_, inplace=False)
+    print(list_)
+    print(sorted_copy)
